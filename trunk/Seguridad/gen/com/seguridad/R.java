fdc02fd3a25ee2cac9320f9f8b835f0dcd33ba59
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-04-21 21:17:06 +0000

    Agregado de imagen de fondo para el botón.

--- trunk/Seguridad/gen/com/seguridad/R.java
+++ trunk/Seguridad/gen/com/seguridad/R.java
@@ -12,7 +12,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int red_button=0x7f020001;
+        public static final int metal_med=0x7f020001;
+        public static final int red_button=0x7f020002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
