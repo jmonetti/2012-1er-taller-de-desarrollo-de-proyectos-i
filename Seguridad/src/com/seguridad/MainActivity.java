8b729438442033f8068d7c4ae925a0c80a290895
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-04-22 18:43:05 +0000

    Agregado de obtención de posición del teléfono utilizando el servicio de la red.

--- Seguridad/src/com/seguridad/MainActivity.java
+++ Seguridad/src/com/seguridad/MainActivity.java
@@ -4,16 +4,19 @@ import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Debug;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
-	private List<Integer> resources;
+public class MainActivity extends Activity implements LocationListener {
+	private List<Integer> resources;	
+	private LocationManager locationManager;
 
     /** Called when the activity is first created. */
     @Override
@@ -23,6 +26,8 @@ public class MainActivity extends Activity {
         this.resources = new LinkedList<Integer>();
         this.resources.add(R.drawable.red_button);
         this.resources.add(R.drawable.red_button_on);
+        
+        this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
     }
     
     public void sendEmergencyCall(View view)
@@ -30,46 +35,29 @@ public class MainActivity extends Activity {
     	Toast.makeText(this, R.string.sending_emergency_call, Toast.LENGTH_SHORT).show();
     	ImageButton btnEmergency = (ImageButton)view;
     	btnEmergency.setClickable(false);
-    	btnEmergency.startAnimation(new BlinkAnimation(btnEmergency,  this.resources, 500));
+    	btnEmergency.startAnimation(new ImageButtonAnimation(btnEmergency,  this.resources, 750));
+    	this.locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);    	
     }
-    
-    public class BlinkAnimation extends Animation implements AnimationListener {
-    	private ImageButton imgButton;
-    	private List<Integer> drawableResources;
-    	private int duration;
-    	private int currentIndex = 0;
+
+	public void onLocationChanged(Location location) {
+		String msg = "Lat:" + location.getLatitude();
+		msg += " - Long:" + location.getLongitude();
+		msg += " - Acc:" + location.getAccuracy();
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
 		
-		public BlinkAnimation(View view, List<Integer> drawableResources, int duration) {
-			this(view);
-			this.drawableResources = drawableResources;
-			this.duration = duration;
-			this.setRepeatCount(this.drawableResources.size() * 50);
-			this.setDuration(this.duration);
-		}
-    	
-    	public BlinkAnimation(View view) {
-    		this.imgButton = (ImageButton) view;
-    		this.setInterpolator(new AccelerateInterpolator());
-    		this.setAnimationListener(this);
-    	}
+	}
 
-		public void onAnimationEnd(Animation animation) {
-			
-		}
-
-		public void onAnimationRepeat(Animation animation) {
-			this.imgButton.setImageResource(this.drawableResources.get(this.getNextIndex()));
-		}
-
-		public void onAnimationStart(Animation animation) {
-			this.currentIndex = 0;
-		}
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
 		
-		private int getNextIndex() {
-			if(++this.currentIndex >= this.drawableResources.size())
-				this.currentIndex = 0;
+	}
 
-			return this.currentIndex;
-		}		
-    }
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
 }
