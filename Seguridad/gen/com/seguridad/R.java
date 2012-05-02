df6b6de3770e558e012affafaca0ac5912c2d45e
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-05-02 17:39:28 +0000

    Agregado de menu (sin funcionalidad). Actualización de presentacion.

--- Seguridad/gen/com/seguridad/R.java
+++ Seguridad/gen/com/seguridad/R.java
@@ -16,13 +16,22 @@ public final class R {
         public static final int red_button=0x7f020002;
         public static final int red_button_on=0x7f020003;
     }
+    public static final class id {
+        public static final int help=0x7f060000;
+        public static final int settings=0x7f060001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int menu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int emergency=0x7f040001;
         public static final int emergency_server_url=0x7f040003;
+        public static final int help=0x7f040004;
         public static final int sending_emergency_call=0x7f040002;
+        public static final int settings=0x7f040005;
     }
 }
