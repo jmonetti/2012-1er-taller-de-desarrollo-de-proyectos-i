a8c4f5f3ff072d33fa499594463f7e2c1b4aaaa4
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-05-02 17:39:28 +0000

    Agregado de menu (sin funcionalidad). Actualización de presentacion.

--- trunk/Seguridad/src/com/seguridad/MainActivity.java
+++ trunk/Seguridad/src/com/seguridad/MainActivity.java
@@ -7,6 +7,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -37,10 +39,17 @@ public class MainActivity extends Activity {
 		ImageButton btnEmergency = (ImageButton) view;
 		btnEmergency.setClickable(false);
 		btnEmergency.startAnimation(new ImageButtonAnimation(btnEmergency,
-				this.resources, 750));
+				this.resources, 500));
 
 		this.locationManager.requestLocationUpdates(
 				LocationManager.NETWORK_PROVIDER, 0, 0,
 				this.locationNotificator);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.menu, menu);
+	    return true;
 	}
 }
