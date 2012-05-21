0a1a1df76c6c5a0c58b030e73aa0ea3915c3a332
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-05-21 20:24:46 +0000

    Agregado de string de conección para la base de datos de MongoDB.

--- webapp/index.php
+++ webapp/index.php
@@ -1,21 +1,21 @@
-<?
-	$mongo = new Mongo();
+<?php
+	$dbconn = "mongodb://root:JZrDH8OLFGoAhEkJSig3@seguridad-jfacorro-db-0.dotcloud.com:24854";
+	$mongo = new Mongo($dbconn);
 	$db = $mongo->urban_security;
 ?>
 <html>
-<head>
-<meta name="author" content="Kai Oswald Seidler">
-<meta http-equiv="cache-control" content="no-cache">
-<title>Urban-Security</title>
-<script type="text/javascript" 
-	src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAJ-lN0BVnxuQ06N3YUyFS_iUA8kuoFYVI&sensor=false">
-</script>
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" src="js/maps.js"></script>
-</head>
+	<head>
+		<meta http-equiv="cache-control" content="no-cache">
+		<title>Urban-Security</title>
+		<script type="text/javascript" 
+			src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAJ-lN0BVnxuQ06N3YUyFS_iUA8kuoFYVI&sensor=false">
+		</script>
+		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="js/maps.js"></script>
+	</head>
 	<body>
 		<div style="float:left">
-		<?
+		<?php
 			$cursor = $db->emergencies->find();
 			// iterate through the results
 			if($cursor)
@@ -28,15 +28,15 @@
 					$location = "{'lat':" . $obj["lat"] . ", 'long':".$obj["long"]."}";
 					?>
 					<script type="text/javascript">
-						mapHandler.markers.push(<? echo $location; ?>);
+						mapHandler.markers.push(<?php echo $location; ?>);
 					</script>
-					<?
+					<?php
 				}
 			}
 		?>
 		</div>
 		<div id="map_canvas" style="width:700px; height:500px; float:right;"></div>
-		<?
+		<?php
 			if(array_key_exists("data", $_REQUEST))
 			{
 				$json = $_REQUEST["data"];
