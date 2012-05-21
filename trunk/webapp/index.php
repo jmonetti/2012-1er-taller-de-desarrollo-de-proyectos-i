a0f9fe15a9b84946891ec6230d3e583a49a46d13
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-05-21 17:56:12 +0000

    Agregado de jquery a la página de prueba de concepto utilizando mongodb y PHP.

--- trunk/webapp/index.php
+++ trunk/webapp/index.php
@@ -10,64 +10,74 @@
 <script type="text/javascript" 
 	src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAJ-lN0BVnxuQ06N3YUyFS_iUA8kuoFYVI&sensor=false">
 </script>
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
 <script type="text/javascript">
-	var map = null
-	function initialize() {
+var mapHandler = {
+	map : null,
+	markers : [],
+	initialize : function() {
 		var myOptions = {
 			center: new google.maps.LatLng(-34.54523, -58.4710496),
 			zoom: 16,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-		var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	}
-	function addMarker(location) {
-		//alert(location.lat + ':' + location.long);
+		mapHandler.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		// cargar todas las posiciones
+		for(index in mapHandler.markers) {
+			mapHandler.addMarker(mapHandler.markers[index]);
+		}
+	},
+	addMarker : function (location) {
 		var image = 'images/alert.png';
 		var myLatLng = new google.maps.LatLng(location.lat, location.long);
 		var beachMarker = new google.maps.Marker({
 			position: myLatLng,
-			map: map,
+			map: mapHandler.map,
 			icon: image
 		});
 	}
+}
+$(function(){
+	mapHandler.initialize();
+});
 </script>
 </head>
-<body bgcolor=#ffffff onload="initialize()">
-<div style="float:left">
-<?
-	$cursor = $db->emergencies->find();
-	// iterate through the results
-	if($cursor)
-	{
-		foreach ($cursor as $obj) {
-			foreach ($obj as $value)
+	<body>
+		<div style="float:left">
+		<?
+			$cursor = $db->emergencies->find();
+			// iterate through the results
+			if($cursor)
 			{
-				echo $value . "<br>";
+				foreach ($cursor as $obj) {
+					foreach ($obj as $value)
+					{
+						echo $value . "<br>";
+					}
+					$location = "{'lat':" . $obj["lat"] . ", 'long':".$obj["long"]."}";
+					?>
+					<script type="text/javascript">
+						mapHandler.markers.push(<? echo $location; ?>);
+					</script>
+					<?
+				}
 			}
-			$location = "{'lat':" . $obj["lat"] . ", 'long':".$obj["long"]."}";
-			?>
-			<script type="text/javascript">
-				addMarker(<? echo $location; ?>);
-			</script>
-			<?
-		}
-	}
-?>
-</div>
-	<div id="map_canvas" style="width:700px; height:500px; float:right;"></div>
-	<?
-		if(array_key_exists("data", $_REQUEST))
-		{
-			$json = $_REQUEST["data"];
-			echo $json;
-			$data = json_decode ($json, true);
-			var_dump($data);
-			$db->emergencies->insert($data);
-		}
-		else
-		{
-			echo "No data";
-		}		
-	?>
-</body>
+		?>
+		</div>
+		<div id="map_canvas" style="width:700px; height:500px; float:right;"></div>
+		<?
+			if(array_key_exists("data", $_REQUEST))
+			{
+				$json = $_REQUEST["data"];
+				echo $json;
+				$data = json_decode ($json, true);
+				var_dump($data);
+				$db->emergencies->insert($data);
+			}
+			else
+			{
+				echo "No data";
+			}
+		?>
+	</body>
 </html>
---
+++ trunk/webapp/info.php
@@ -0,0 +1,4 @@
+<?php
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+phpinfo();
+?>
