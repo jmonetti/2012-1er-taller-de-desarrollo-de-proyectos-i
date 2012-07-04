99078ca0ec567db51b6835010dc3a96653085538
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-07-04 02:32:14 +0000

    Agregado header, menu y footer.

---
+++ webapp/footer.php
@@ -0,0 +1,13 @@
+<div id="footer">
+	<ul>
+		<li><a href="index.php">Inicio</a>
+		</li>
+		<li><a href="info.php">Informacion Util</a>
+		</li>
+		<li><a href="foro.php">Foro</a>
+		</li>
+		<li><a href="faqs.php">FAQs</a>
+		</li>
+		<li class="last">Seguridad Urbana &copy; 2012</li>
+	</ul>
+</div>
