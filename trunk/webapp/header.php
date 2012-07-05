c10f5149c9ad667f1be7348ff40cbf282bc5fd9d
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-07-05 17:11:20 +0000

    Agregado de página de Download.

--- trunk/webapp/header.php
+++ trunk/webapp/header.php
@@ -7,6 +7,7 @@
 	<li><a href="info.php">Informacion Util</a></li>
 	<li><a href="forum.php">Foro</a></li>
 	<li><a href="faqs.php">FAQs</a></li>
+	<li><a href="download.php">Download</a></li>
 	<li class="login"><a href="login.php">Log-in</a></li>
 </ul>
 <div class="clear"></div>
