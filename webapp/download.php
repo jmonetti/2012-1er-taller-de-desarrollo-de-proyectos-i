d60efaffd51fc5c7ac32c76e95913a589dd6df0c
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-07-05 17:11:20 +0000

    Agregado de página de Download.

---
+++ webapp/download.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<meta http-equiv="cache-control" content="no-cache">
+<title>Seguridad</title>
+<script type="text/javascript"
+	src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAJ-lN0BVnxuQ06N3YUyFS_iUA8kuoFYVI&sensor=false">
+		</script>
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/maps.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+<script type="text/javascript">
+	$(function(){
+		mapHandler.init();
+        main.refresh();
+	});
+</script>
+<link href="css/default.css" type="text/css" rel="Stylesheet" />
+</head>
+<body>
+	<div id="main">
+		<?php include('header.php'); ?>
+		<div id="content">
+			<h2>Download</h2>
+			<a href="http://2012-1er-taller-de-desarrollo-de-proyectos-i.googlecode.com/svn/trunk/android/bin/Seguridad.apk">
+			<img src="images/android.jpg" />
+			</a>
+			<div class="clear"></div>
+		</div>
+		<?php include('footer.php'); ?>
+	</div>
+</body>
+</html>
