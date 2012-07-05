505003328af08859692ba786ab89816e7d296fce
juan.facorro <juan.facorro@b323dadd-20ad-c036-299c-88f077ac5584>
2012-07-05 18:24:12 +0000

    Modificación del título de las páginas.

--- trunk/webapp/includes/Configuration-prod.php
+++ trunk/webapp/includes/Configuration-prod.php
@@ -2,6 +2,7 @@
 
 class Configuration {
 	public static $ConnectionString = "mongodb://root:qFGYsIeuqLbupQId7QPu@seguridad-jfacorro-db-0.dotcloud.com:24854";
+	public static $Title = 'Panic Dial Button';
 }
 
 ?>
